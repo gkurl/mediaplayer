6c16f4c1f4e87361b23749edad11cb67fdde038d
gkurl <gkurl@hotmail.co.uk>
2018-02-13 17:27:03 +0000

    added spotify provider in Socialite classes

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -15,7 +15,7 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         'App\Events\Event' => [
             'App\Listeners\EventListener',
-            \SocialiteProviders\Manager\SocialiteWasCalled::class =>[        'SocialiteProviders\Spotify\SpotifyExtendSocialite@handle',
+            \SocialiteProviders\Manager\SocialiteWasCalled::class =>[
                 'SocialiteProviders\Spotify\SpotifyExtendSocialite@handle',
             ],
         ],
