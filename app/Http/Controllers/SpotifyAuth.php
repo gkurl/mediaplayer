e299ac91ccdd2f1cbf39b8c5ea68657d7a82250d
gkurl <gkurl@hotmail.co.uk>
2018-03-05 15:38:14 +0000

    think logic for auth and access/refresh is almost done

--- app/Http/Controllers/SpotifyAuth.php
+++ app/Http/Controllers/SpotifyAuth.php
@@ -71,56 +71,63 @@ class SpotifyAuth extends Controller
 
 
 
-    public function retrieveTokens(Request $request)
+    public function retrieveTokens(\GuzzleHttp\Client $tryUrl, Request $request)
     {
+        //Start Spotify API session
+
+        $client_id = env('SPOTIFY_KEY');
+        $client_secret = env('SPOTIFY_SECRET');
+        $redirect_uri = env('SPOTIFY_REDIRECT_URI');
+
+        //instantiate session for Spotify web API
+
+        $session = new SpotifyWebAPI\Session(
+
+            $client_id,
+            $client_secret,
+            $redirect_uri
+        );
+
+        //Obtain Refresh token from DB
+
         $refreshToken = \App\User::pluck('refresh_token')->where('email', $request->post('email'));
 
+        //Do some checks to see if token is there or not to determine if already existing user
 
-        if(empty($refreshToken->refresh_token)) {
+        if (empty($refreshToken->refresh_token)){
 
             redirect('/login/spotify');
-        }}
 
-     /*   } elseif (isset($refreshToken)){
+        } elseif($refreshToken->refresh_token){
 
-            $session = new SpotifyWebAPI\Session(
+            $accessToken = \App\User::pluck('access_token')->where('email', $request->post('email'));
 
-                $client_id = env('SPOTIFY_KEY'),
-                $client_secret = env('SPOTIFY_SECRET'),
-                $redirect_url = env('SPOTIFY_REDIRECT_URI')
-            );
+            //Check for timeout if logging in after long time, if so, get new access token
 
+            try {
+                $tryUrl->get("https://api.spotify.com/v1/me/top/",  ['headers' => [
+                    'Accept' => 'application/json',
+                    'Authorization' => 'Bearer ' . $accessToken->access_token
+                ],
+            ]);
 
-            $newAccessToken = $session->refreshAccessToken($refreshToken);
+            } catch (\Exception $e){
 
-            \App\User::where('refresh_token', $refreshToken)->update(['refresh_token' => $newAccessToken]);
+                $session->refreshAccessToken($refreshToken->refresh_token);
+
+                $accessToken = $session->getAccessToken();
+
+                //Update access token in DB
+
+                \App\User::pluck('access_token')->where('email', $request->post('email')->update('access_token', $accessToken));
+
+                $api = new SpotifyWebAPI\SpotifyWebAPI();
+                $api->setAccessToken($accessToken);
+
+            }
         }
 
 
-        return view('mystats');*/
-
-
-   /* public function apiWrapper(){
-
-        $api = new SpotifyWebAPI\SpotifyWebAPI();
-
-        //Fetch saved access tokens
-
-        $accessToken = \App\Tokens::where('access_token')->first();
-
-
-        $api->setAccessToken($accessToken);
-
-
-    }*/
-
-
-
-
-        public function denied()
-        {
-
-            return view('denied');
         }
 
 
